355cd1468762a6a02f0e05f1d7c2e900aca8e20c
shreyjha <shreyjwins@gmail.com>
2024-03-09 02:52:12 +0530

    added some functionality to login button and designs to a block.

---
+++ index.js
@@ -0,0 +1,39 @@
+let loginBtn = document.getElementById("head-3")
+loginBtn.addEventListener("mouseover", loginOpen)
+loginBtn.addEventListener("mouseout", loginClose)
+
+let usericonLb = document.getElementById("user-icon")
+let loginLb = document.getElementById("login-btn")
+let loginarrowLb = document.getElementById("login-arrow")
+
+let loginPopup = document.getElementById("login-popup")
+
+function loginOpen() {
+    loginBtn.style.backgroundColor = "rgb(42, 91, 236)"
+    usericonLb.style.backgroundColor = "rgb(42, 91, 236)"
+    loginLb.style.backgroundColor = "rgb(42, 91, 236)"
+    loginarrowLb.style.backgroundColor = "rgb(42, 91, 236)"
+
+    usericonLb.style.color = "white"
+    loginLb.style.color = "white"
+    loginarrowLb.style.color = "white"
+
+    loginarrowLb.style.transform = "rotate(270deg)"
+
+    loginPopup.style.display = "block"
+}
+
+function loginClose() {
+    loginBtn.style.backgroundColor = "white"
+    usericonLb.style.backgroundColor = "white"
+    loginLb.style.backgroundColor = "white"
+    loginarrowLb.style.backgroundColor = "white"
+
+    usericonLb.style.color = "rgb(36, 36, 36)"
+    loginLb.style.color = "rgb(36, 36, 36)"
+    loginarrowLb.style.color = "rgb(36, 36, 36)"
+
+    loginarrowLb.style.transform = "rotate(90deg)"
+
+    loginPopup.style.display = "none"
+}
